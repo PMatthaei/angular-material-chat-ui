76529d3bd15d7db9eac55832f1ea6427429b6f28
Patrick Matthäi <matthaei@cip.ifi.lmu.de>
2020-01-03 17:14:04 +0100

    Small improvements on typing feature formcontrol

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -2,9 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { ChatService } from '../services/chat.service';
 import { ActivatedRoute } from '@angular/router';
 import { Observable, Subject } from 'rxjs';
-import { tap, debounceTime, throttleTime, filter } from 'rxjs/operators';
+import { tap, debounceTime, throttleTime, filter, switchMap } from 'rxjs/operators';
 import { AuthService } from '../services/auth.service';
 import { from } from 'rxjs';
+import {
+  FormControl,
+  FormGroup,
+  FormBuilder
+} from '@angular/forms';
 
 @Component({
   selector: 'app-chat',
@@ -15,13 +20,18 @@ export class ChatComponent implements OnInit {
   chat$: Observable<any>;
   newMsg: string;
 
-  isTyping: Subject<string> = new Subject<string>();
+  messageControl: FormControl;
+  chatForm: FormGroup;
 
   constructor(
     public cs: ChatService,
     private route: ActivatedRoute,
-    public auth: AuthService
-  ) { }
+    public auth: AuthService,
+    private fb: FormBuilder
+  ) {
+    this.messageControl = new FormControl();
+    this.chatForm = this.fb.group({ message: this.messageControl });
+  }
 
   ngOnInit() {
     const chatId = this.route.snapshot.paramMap.get('id');
@@ -29,17 +39,17 @@ export class ChatComponent implements OnInit {
     this.chat$ = this.cs.buildChat(source).pipe(tap(v => this.scrollBottom()));
     this.scrollBottom();
 
-    this.isTyping.asObservable().pipe(filter(data => data != ''), throttleTime(1400)).subscribe((data) => {
+    this.messageControl.valueChanges.pipe(filter(data => data != ''), throttleTime(1400)).subscribe((data) => {
       this.cs.sendIsTyping(chatId)
     });
 
-    this.isTyping.asObservable().pipe(filter(data => data != ''), debounceTime(1500)).subscribe((data) => {
+    this.messageControl.valueChanges.pipe(filter(data => data != ''), debounceTime(1500)).subscribe((data) => {
       this.cs.deleteIsTyping(chatId)
     });
   }
 
-  getUserById(typerId){
-   return this.getUserName(this.cs.getUserById(typerId))
+  getUserById(typerId) {
+    return this.getUserName(this.cs.getUserById(typerId))
   }
 
   getCreatedDate(msg) {
@@ -48,10 +58,6 @@ export class ChatComponent implements OnInit {
 
   getUserName(user) {
     return user.realName ? user.realName : user.name;
-  }
-
-  setTypingActive(value) {
-    this.isTyping.next(value)
   }
 
   submit(chatId) {
