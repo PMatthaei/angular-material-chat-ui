f6a9d09e1ddefc44d42b20e34687b16f906ff877
Patrick Matthäi <matthaei@cip.ifi.lmu.de>
2020-01-09 11:23:12 +0100

    Cloud funtions dummy and small fixes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,17 +1,32 @@
-const express = require('express');
-const cors = require('cors');
 
-const app = express();
+'use strict';
 
-// Automatically allow cross-origin requests
-app.use(cors({ origin: true }));
+const functions = require('firebase-functions');
 
-// Add middleware to authenticate requests
-//app.use(myMiddleware);
+const moment = require('moment');
+const cors = require('cors')({
+  origin: true,
+});
 
-// build multiple CRUD interfaces:
-app.get('/users/:id', (req: any, res:any) => res.send("Hello"));
+exports.users = functions.https.onRequest((req: any, res: any) => {
 
+  if (req.method === 'PUT') {
+    return res.status(403).send('Forbidden!');
+  }
 
-// Expose Express API as a single Cloud Function:
-exports.widgets = functions.https.onRequest(app);
+  // TODO:
+  // - Get requesting user data -> especially roles, company, chat, participants and permissions
+  // - Implement rules:
+  // If allowed to access public data: Return basic user data and anonymous data
+  // If allowed to access secure data: Return all user data
+  return cors(req, res, () => {
+
+    const data = {
+        name: "test",
+        email: "test"
+    };
+
+    console.log('Sending user data:', data);
+    res.status(200).send(data);
+  });
+});
